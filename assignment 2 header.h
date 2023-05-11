c4f097f056557ca854ebc71df1e588eebdc452e8
2020104236 <127279955+2020104236@users.noreply.github.com>
2023-05-11 22:15:02 +0900

    Create assignment 2 header.h

---
+++ assignment 2 header.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+using namespace std;
+class FC {
+public:
+	FC() {};
+	FC(float cc);
+	float c, f;
+	float getF();
+	float getC();
+};
+class city {
+public:
+	city() {}
+	city(char*) {}
+	FC fc[12]; ifstream xx; float c;
+};
